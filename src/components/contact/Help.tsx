407857c3a535c9b0a8a82b7342f677bfce77a76b
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-06-28 17:26:35 +0600

    massage form added

--- src/components/contact/Help.tsx
+++ src/components/contact/Help.tsx
@@ -1,6 +1,7 @@
 import SearchBar from '../SearchBar'
 import Categories from './Categories'
 import ContactOptions from './ContactOptions'
+import MassageForm from './MassageForm'
 
 const Help = () => {
   return (
@@ -9,7 +10,7 @@ const Help = () => {
             <p className='text-5xl font-semibold'>We’re here to help</p>
             <p className='text-xl font-normal text-gray-400'>Whether you have a question or need assistance, getting in touch with us is simple.</p>
         </div>
-        <div className='pt-5'>
+        <div className='py-5'>
             <SearchBar/>
         </div>
         <div>
@@ -17,6 +18,9 @@ const Help = () => {
         </div>
         <div className='py-5'>
             <ContactOptions/>
+        </div>
+        <div className='py-5'>
+            <MassageForm/>
         </div>
     </div>
   )
