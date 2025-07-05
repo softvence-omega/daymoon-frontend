e02c0f18f7e403c21439a3def1460af54f74dac5
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-07-05 10:42:15 +0600

    Inquiry Details working

---
+++ src/pages/SellerDashboard/SellerInquiriesDetails.tsx
@@ -0,0 +1,11 @@
+import InquiryDetails from "@/components/SellerDashboard/SellerInquirie/InquiryDetails";
+
+const SellerInquiriesDetails = () => {
+  return (
+    <div>
+      <InquiryDetails />
+    </div>
+  );
+};
+
+export default SellerInquiriesDetails;
