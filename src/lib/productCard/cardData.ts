ae900017d77cd95aee968c6de973a9de2c77f441
rakibwebdev23 <rh.rakibhasan365@gmail.com>
2025-07-05 12:13:17 +0600

    Company overview page complete

--- src/lib/productCard/cardData.ts
+++ src/lib/productCard/cardData.ts
@@ -13,17 +13,18 @@ export interface ProductType {
   priceRange: string;
   rating: number;
   moq: string;
+  category:string;
 }
 
 export const products: ProductType[] = [
   {
     id: 1,
-    title:
-      "Bluetooth Wireless Earbuds with Deep Bass, 40H Playtime, LED Display, IP7 Rating, Mic for Phone and Android, Black.",
+    title: "Bluetooth Wireless Earbuds with Deep Bass, 40H Playtime, LED Display, IP7 Rating, Mic for Phone and Android, Black.",
     images: [prdt1, prdt2],
     priceRange: "$20-$28",
     rating: 4.2,
     moq: "MOQ : 10",
+    category: "Electronics", 
   },
   {
     id: 2,
@@ -32,6 +33,7 @@ export const products: ProductType[] = [
     priceRange: "$12-$18",
     rating: 4.5,
     moq: "MOQ : 20",
+    category: "Electronics",
   },
   {
     id: 3,
@@ -40,6 +42,7 @@ export const products: ProductType[] = [
     priceRange: "$30-$35",
     rating: 4.8,
     moq: "MOQ : 5",
+    category: "Clothing",
   },
   {
     id: 4,
@@ -48,6 +51,7 @@ export const products: ProductType[] = [
     priceRange: "$45-$60",
     rating: 4.6,
     moq: "MOQ : 8",
+    category: "Electronics",
   },
   {
     id: 5,
@@ -56,6 +60,7 @@ export const products: ProductType[] = [
     priceRange: "$25-$40",
     rating: 4.4,
     moq: "MOQ : 15",
+    category: "Electronics",
   },
   {
     id: 6,
@@ -64,6 +69,7 @@ export const products: ProductType[] = [
     priceRange: "$80-$120",
     rating: 4.7,
     moq: "MOQ : 12",
+    category: "Electronics",
   },
   {
     id: 7,
@@ -72,6 +78,7 @@ export const products: ProductType[] = [
     priceRange: "$80-$120",
     rating: 4.7,
     moq: "MOQ : 12",
+    category: "Electronics",
   },
   {
     id: 8,
@@ -80,6 +87,7 @@ export const products: ProductType[] = [
     priceRange: "$80-$120",
     rating: 4.7,
     moq: "MOQ : 12",
+    category: "Electronics",
   },
   {
     id: 9,
@@ -88,5 +96,33 @@ export const products: ProductType[] = [
     priceRange: "$30-$35",
     rating: 4.8,
     moq: "MOQ : 5",
+    category: "Laptop",
+  },
+  {
+    id: 10,
+    title: "Stylish Analog Watch for Men - Waterproof & Durable.",
+    images: [prdt5, prdt6],
+    priceRange: "$30-$35",
+    rating: 4.8,
+    moq: "MOQ : 5",
+    category: "Computer",
+  },
+  {
+    id: 11,
+    title: "Stylish Analog Watch for Men - Waterproof & Durable.",
+    images: [prdt5, prdt6],
+    priceRange: "$30-$35",
+    rating: 4.8,
+    moq: "MOQ : 5",
+    category: "Laptop",
+  },
+  {
+    id: 12,
+    title: "Stylish Analog Watch for Men - Waterproof & Durable.",
+    images: [prdt5, prdt6],
+    priceRange: "$30-$35",
+    rating: 4.8,
+    moq: "MOQ : 5",
+    category: "Mobile",
   },
 ];
