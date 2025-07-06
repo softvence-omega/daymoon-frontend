5326847d9376bfba1dd54a728aa320dfb57c486f
zxayed <zzayedghost@gmail.com>
2025-07-06 09:10:35 +0600

    added navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import CommonWrapper from "../common/CommonWrapper";
 
 const Home = () => {
-  return <CommonWrapper>home sweet home</CommonWrapper>;
+  return (
+    <CommonWrapper>
+      <div className="h-screen text-sunset-orange">Home</div>
+    </CommonWrapper>
+  );
 };
 
 export default Home;
